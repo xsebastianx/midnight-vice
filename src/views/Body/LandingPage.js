3ff67b8a969ea1742d8163da724212d5dd72a87c
French Stuff <legarreta.french@gmail.com>
2020-03-02 19:45:42 -0500

    added business input field

--- src/views/Body/LandingPage.js
+++ src/views/Body/LandingPage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import M from 'materialize-css';
 
 // reactstrap components
 import {
@@ -38,14 +39,47 @@ function LandingPage() {
           <Container>
             <Row>
               <Col className="ml-auto mr-auto text-center" md="8">
-                <h2 className="title">Who we are?</h2>
-                <h5 className="description">
-                  According to the National Oceanic and Atmospheric
-                  Administration, Ted, Scambos, NSIDClead scentist, puts the
-                  potentially record low maximum sea ice extent tihs year down
-                  to low ice extent in the Pacific and a late drop in ice extent
-                  in the Barents Sea.
-                </h5>
+                <h2 className="title">Add Your Business Details</h2>
+                <div class="row">
+                  <form class="col s12">
+                    <div class="row">
+                      <div class="input-field col s3">
+                        <i class="material-icons prefix">account_circle</i>
+                        <input id="icon_prefix" type="text" class="validate" />
+                        <label for="icon_prefix">Company</label>
+                      </div>
+                      <div class="input-field col s3">
+                        <i class="material-icons prefix">phone</i>
+                        <input id="icon_telephone" type="tel" class="validate" />
+                        <label for="icon_telephone">Telephone</label>
+                      </div>
+                      <div class="input-field col s3">
+                        <i class="material-icons prefix">devices</i>
+                        <input id="icon_prefix" type="text" class="validate" />
+                        <label for="icon_prefix">Website</label>
+                      </div>
+                    </div>
+                  </form>
+                </div>
+
+                <div class="row">
+                  <form class="col s12">
+                    <div class="row">
+                      <div class="input-field col s12">
+                        <input placeholder="Address" id="first_name" type="text" class="validate" />
+                        <label for="textarea1">Location</label>
+                      </div>
+                    </div>
+                  </form>
+                </div>
+                <form class="col s12">
+                  <div class="row">
+                    <div class="input-field col s12">
+                      <input placeholder="Friday 6pm-2am Saturday 8pm-3am" id="first_name" type="text" class="validate" />
+                      <label for="textarea1">Hours of Operation</label>
+                    </div>
+                  </div>
+                </form>
               </Col>
             </Row>
             <div className="separator separator-primary"></div>
