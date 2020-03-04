49bfb91b4dd484b936d8b2ee24b7d01d2babd57f
French Stuff <legarreta.french@gmail.com>
2020-03-04 13:54:55 -0500

    removed redirect page

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@
 */
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import { BrowserRouter, Route, Switch,} from "react-router-dom";
 
 // styles for this kit
 import "assets/css/bootstrap.min.css";
