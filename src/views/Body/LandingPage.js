0c161e077bbc73b0288dcb4d4ee110bc84a15103
French Stuff <legarreta.french@gmail.com>
2020-03-04 19:34:36 -0500

    Revert "removed demo info"
    This reverts commit 4f8b599670124b6e3a375746a458d35190fe37db.

--- src/views/Body/LandingPage.js
+++ src/views/Body/LandingPage.js
@@ -5,7 +5,9 @@ import React from "react";
 import {
   Button,
   Input,
-  
+  InputGroupAddon,
+  InputGroupText,
+  InputGroup,
   Container,
   Row,
   Col
@@ -155,8 +157,200 @@ function LandingPage() {
         </div>
         <div className="section section-team text-center">
           <Container>
-      
-                  
+            <h2 className="title">Here is our team</h2>
+            <div className="team">
+              <Row>
+                <Col md="4">
+                  <div className="team-player">
+                    <img
+                      alt="..."
+                      className="rounded-circle img-fluid img-raised"
+                      src={require("assets/img/avatar.jpg")}
+                    ></img>
+                    <h4 className="title">Romina Hadid</h4>
+                    <p className="category text-info">Model</p>
+                    <p className="description">
+                      You can write here details about one of your team members.
+                      You can give more details about what they do. Feel free to
+                      add some{" "}
+                      <a href="#pablo" onClick={e => e.preventDefault()}>
+                        links
+                      </a>{" "}
+                      for people to be able to follow them outside the site.
+                    </p>
+                    <Button
+                      className="btn-icon btn-round"
+                      color="info"
+                      href="#pablo"
+                      onClick={e => e.preventDefault()}
+                    >
+                      <i className="fab fa-twitter"></i>
+                    </Button>
+                    <Button
+                      className="btn-icon btn-round"
+                      color="info"
+                      href="#pablo"
+                      onClick={e => e.preventDefault()}
+                    >
+                      <i className="fab fa-instagram"></i>
+                    </Button>
+                    <Button
+                      className="btn-icon btn-round"
+                      color="info"
+                      href="#pablo"
+                      onClick={e => e.preventDefault()}
+                    >
+                      <i className="fab fa-facebook-square"></i>
+                    </Button>
+                  </div>
+                </Col>
+                <Col md="4">
+                  <div className="team-player">
+                    <img
+                      alt="..."
+                      className="rounded-circle img-fluid img-raised"
+                      src={require("assets/img/ryan.jpg")}
+                    ></img>
+                    <h4 className="title">Ryan Tompson</h4>
+                    <p className="category text-info">Designer</p>
+                    <p className="description">
+                      You can write here details about one of your team members.
+                      You can give more details about what they do. Feel free to
+                      add some{" "}
+                      <a href="#pablo" onClick={e => e.preventDefault()}>
+                        links
+                      </a>{" "}
+                      for people to be able to follow them outside the site.
+                    </p>
+                    <Button
+                      className="btn-icon btn-round"
+                      color="info"
+                      href="#pablo"
+                      onClick={e => e.preventDefault()}
+                    >
+                      <i className="fab fa-twitter"></i>
+                    </Button>
+                    <Button
+                      className="btn-icon btn-round"
+                      color="info"
+                      href="#pablo"
+                      onClick={e => e.preventDefault()}
+                    >
+                      <i className="fab fa-linkedin"></i>
+                    </Button>
+                  </div>
+                </Col>
+                <Col md="4">
+                  <div className="team-player">
+                    <img
+                      alt="..."
+                      className="rounded-circle img-fluid img-raised"
+                      src={require("assets/img/eva.jpg")}
+                    ></img>
+                    <h4 className="title">Eva Jenner</h4>
+                    <p className="category text-info">Fashion</p>
+                    <p className="description">
+                      You can write here details about one of your team members.
+                      You can give more details about what they do. Feel free to
+                      add some{" "}
+                      <a href="#pablo" onClick={e => e.preventDefault()}>
+                        links
+                      </a>{" "}
+                      for people to be able to follow them outside the site.
+                    </p>
+                    <Button
+                      className="btn-icon btn-round"
+                      color="info"
+                      href="#pablo"
+                      onClick={e => e.preventDefault()}
+                    >
+                      <i className="fab fa-google-plus"></i>
+                    </Button>
+                    <Button
+                      className="btn-icon btn-round"
+                      color="info"
+                      href="#pablo"
+                      onClick={e => e.preventDefault()}
+                    >
+                      <i className="fab fa-youtube"></i>
+                    </Button>
+                    <Button
+                      className="btn-icon btn-round"
+                      color="info"
+                      href="#pablo"
+                      onClick={e => e.preventDefault()}
+                    >
+                      <i className="fab fa-twitter"></i>
+                    </Button>
+                  </div>
+                </Col>
+              </Row>
+            </div>
+          </Container>
+        </div>
+        <div className="section section-contact-us text-center">
+          <Container>
+            <h2 className="title">Want to work with us?</h2>
+            <p className="description">Your project is very important to us.</p>
+            <Row>
+              <Col className="text-center ml-auto mr-auto" lg="6" md="8">
+                <InputGroup
+                  className={
+                    "input-lg" + (firstFocus ? " input-group-focus" : "")
+                  }
+                >
+                  <InputGroupAddon addonType="prepend">
+                    <InputGroupText>
+                      <i className="now-ui-icons users_circle-08"></i>
+                    </InputGroupText>
+                  </InputGroupAddon>
+                  <Input
+                    placeholder="First Name..."
+                    type="text"
+                    onFocus={() => setFirstFocus(true)}
+                    onBlur={() => setFirstFocus(false)}
+                  ></Input>
+                </InputGroup>
+                <InputGroup
+                  className={
+                    "input-lg" + (lastFocus ? " input-group-focus" : "")
+                  }
+                >
+                  <InputGroupAddon addonType="prepend">
+                    <InputGroupText>
+                      <i className="now-ui-icons ui-1_email-85"></i>
+                    </InputGroupText>
+                  </InputGroupAddon>
+                  <Input
+                    placeholder="Email..."
+                    type="text"
+                    onFocus={() => setLastFocus(true)}
+                    onBlur={() => setLastFocus(false)}
+                  ></Input>
+                </InputGroup>
+                <div className="textarea-container">
+                  <Input
+                    cols="80"
+                    name="name"
+                    placeholder="Type a message..."
+                    rows="4"
+                    type="textarea"
+                  ></Input>
+                </div>
+                <div className="send-button">
+                  <Button
+                    block
+                    className="btn-round"
+                    color="info"
+                    href="#pablo"
+                    onClick={e => e.preventDefault()}
+                    size="lg"
+                  >
+                    Send Message
+                  </Button>
+                </div>
+              </Col>
+            </Row>
           </Container>
         </div>
         <DefaultFooter />
