e7aec08cfd3ae4dbdacc7f5b1cb9214698db0507
Sebastian Arnavat <sebastianarnavat@Sebastians-MacBook-Pro.local>
2020-03-07 14:03:35 -0500

    Dark mode is functional

--- src/components/Navbars/HomeNavbar.js
+++ src/components/Navbars/HomeNavbar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+
 // reactstrap components
 import {
   Collapse,
--- src/index.js
+++ src/index.js
@@ -20,10 +20,13 @@ import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Switch,} from "react-router-dom";
 
 // styles for this kit
+import "assets/css/theme.css";
 import "assets/css/bootstrap.min.css";
 import "assets/scss/now-ui-kit.scss";
 import "assets/demo/demo.css";
 import "assets/demo/nucleo-icons-page-styles.css";
+
+
 
 // pages for this kit
 import Index from "views/Index.js";
